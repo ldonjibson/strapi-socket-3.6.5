d2f536e7f8d1f4c7276f6a4cba578bc511a2b308
Olayanju Ajibola <ajibolaolayanju@gmail.com>
2022-01-02 11:56:50 +0100

    migrated the database mildly

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', 'help.db'),
+        client: 'postgres',
+				host: config.host,
+				port: config.port,
+				database: config.database,
+				user: config.user,
+				password: config.password,
+				ssl: false,//{ rejectUnauthorized: false },
       },
       options: {
         useNullAsDefault: true,
